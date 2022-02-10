ba5d740e7528d4ead1047d6f130f653c9876f8db
ggo <gerrygoable@gmail.com>
2022-02-10 11:46:45 -0600

    factor scale outside of generating functions

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,8 @@
-
-// a workout is a sequence of distances and multipliers
-// the distances are a multiple of the pool length
-interface Drill {
-	interval: number
-	reps: number
-
-	rest: number
-}
-
-interface Workout {
-	drills: Drill[]
-}
-
-interface Pool {
-	length: number
-	unit: 'm' | 'y'
-}
-
-interface Endurance {
-  stamina: number
-}
+import {
+	SwimSet,
+	Pool,
+	Workout,
+} from './swimTypes'
 
 function getDivisors(n: number): number[] {
 	const bigDivisors: number[] = []
@@ -39,24 +22,35 @@ function getDivisors(n: number): number[] {
 }
 
 // requires a > b and x in [0, 1]
-function scaleInto(a: number, b:number, x: number) {
+function scaleInto(b:number, x: number) {
+	const a = 0
   return Math.floor(a + ((b - a) * x))
 }
 
+function average(items: number[], weights: number[] | undefined = undefined): number {
+	let w: number[]
+
+	if (weights === undefined) w = items.map( _ => 1)
+	else w = weights
+
+	return items.reduce((res, cur, idx) => res + cur * w[idx])/items.length
+}
+
 function selectUsingScale(candidates: number[], scale: number): number {
-	const idx = scaleInto(0, candidates.length - 1, scale)
+	const idx = scaleInto(candidates.length - 1, scale)
 	return candidates[idx]
 }
 
-function makeDrill(poolLength: number, drillLength: number, endurance: number): Drill {
+function makeSwimSet(poolLength: number, drillLength: number, lengthScale: number): SwimSet {
 	// the lap length is between poolLength and drillLength
 	// and is a multiple of poolLength
 	// and evenly divides drillLength
+	// TODO: support fast sets of poolLength
 	const possibleIntervals = getDivisors(drillLength)
-	  .filter(interval => interval >= poolLength &&
-					 interval % poolLength == 0)
+	  .filter(interval => interval > poolLength &&
+					 interval % (poolLength * 2) == 0)
 	
-	const interval = selectUsingScale(possibleIntervals, endurance)
+	const interval = selectUsingScale(possibleIntervals, lengthScale)
 
 	return {
 		interval,
@@ -65,50 +59,67 @@ function makeDrill(poolLength: number, drillLength: number, endurance: number): 
 	}
 }
 
-// TODO: support finishing drills on the other side of the pool
-function makeDrillDistances(poolSize: number, maxDistance: number): number[] {
+function makeSwimSetDistances(poolSize: number, maxDistance: number): number[] {
 	const distances: number[] = []
 
+
+  // TODO: support finishing drills on the other side of the pool
+	const minDistance = poolSize * 2
+	let candidateDistance = minDistance
 	let i = 1
-	let candidateDistance = poolSize * 2
 	while (candidateDistance <= maxDistance) {
 		distances.push(candidateDistance)
-		candidateDistance = poolSize * 2 * ++i
+		candidateDistance = minDistance * ++i
 	}
 
 	return distances
 }
 
-function makeDrillDistance(poolSize:number, workoutDistance:number, remainingDistance: number, lengthBias:number): number {
-	// we want the distance to be a function of:
-	// - the 'stage' of the workout (warmup, main, cooldown)
-	// - a bias towards length
-	// [poolSize, remaining]
-	const progress = remainingDistance/workoutDistance
-	const peripheryDistance = 1 - Math.abs(0.5 - progress) * 2
-
-	const possibleDistances = makeDrillDistances(poolSize, remainingDistance)
+function makeSwimSetDistance(poolSize:number, remainingDistance: number, lengthScale:number): number {
+	// [poolSize, remainingDistance]
+	const possibleDistances = makeSwimSetDistances(poolSize, remainingDistance)
 	if (possibleDistances.length === 0) {
 		console.log('oops! no drill distances could be generated')
 		return remainingDistance
 	}
+	console.log(possibleDistances)
 
-	// skew towards smaller options at the beginning
-	// and towards larger options in the end, because:
-	// - initially the options are dominated by larger numbers
-	// - there are few options near the end
-	const scale = ((0.5 + 0.5 * lengthBias) + peripheryDistance + (1 - progress))/3
-	return selectUsingScale(possibleDistances, scale)
+	return selectUsingScale(possibleDistances, lengthScale)
 }
 
 function makeWorkout(pool: Pool, workoutDistance: number, endurance: number): Workout {
-	const drills: Drill[] = []
+	console.log(pool, workoutDistance, endurance)
+	const drills: SwimSet[] = []
 	let remainingDistance = workoutDistance
 
 	while (remainingDistance > 0) {
-		const drillDistance = makeDrillDistance(pool.length, workoutDistance, remainingDistance, endurance)
+		// skew towards smaller options at the beginning
+		// and towards larger options in the end, because:
+		// - initially the options are dominated by larger numbers
+		// - there are few options near the end
+		
+	  const enduranceFactor = 0.5 + 0.5 * endurance
+		const progressFactor = 1 - remainingDistance/workoutDistance
 
-		drills.push(makeDrill(pool.length, drillDistance, endurance))
+		const midPoint = workoutDistance / 2
+		const midSeparation = Math.abs(midPoint - (workoutDistance - remainingDistance))
+		const peripheryDistanceFactor = 1 - midSeparation/midPoint
+
+		console.log(
+			'endurance: ', enduranceFactor,
+			'periphery: ', peripheryDistanceFactor,
+			'progress: ', progressFactor
+		)
+
+	  const scale = average([
+			enduranceFactor,
+			peripheryDistanceFactor,
+			progressFactor
+		])
+		const drillDistance = makeSwimSetDistance(pool.length, remainingDistance, scale)
+
+
+		drills.push(makeSwimSet(pool.length, drillDistance, scale))
 
 		remainingDistance -= drillDistance
 	}
@@ -118,25 +129,27 @@ function makeWorkout(pool: Pool, workoutDistance: number, endurance: number): Wo
 
 const form = document.querySelector('form')
 const result = document.getElementById('result')
-form.addEventListener('submit', event => {
-	event.preventDefault()
+if (form !== null && result !== null) {
+	form.addEventListener('submit', event => {
+		event.preventDefault()
 
-	const {
-		0: {value: poolLengthStr},
-		1: {value: workoutDistanceStr},
-		2: {value: enduranceStr},
-	} = event.target
+		const {
+			0: {value: poolLengthStr},
+			1: {value: workoutDistanceStr},
+			2: {value: enduranceStr},
+		} = (event.target as any)
 
-	const workout = makeWorkout(
-			{
-				length: parseInt(poolLengthStr),
-				units: 'm'
-      },
-			parseInt(workoutDistanceStr),
-			parseInt(enduranceStr)/100
-		))
+		const workout = makeWorkout(
+				{
+					length: parseInt(poolLengthStr),
+					unit: 'm'
+				},
+				parseInt(workoutDistanceStr),
+				(parseInt(enduranceStr) - 50)/100
+			)
 
-	result.textContent = workout.drills
-	  .map(({ interval, reps }) => `${reps} of ${interval}`)
-		.join(', ')
-})
+		result.textContent = workout.drills
+			.map(({ interval, reps }) => `${reps} of ${interval}`)
+			.join(', ')
+	})
+}
